1e9a8a447ef1aefa450ec6105126ca6f07b50258
araij <araijn@gmail.com>
2020-01-05 15:54:31 +0900

    Refactor

--- docs/nammd.js
+++ docs/nammd.js
@@ -1,5 +1,9 @@
 const revealCdn = "//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.8.0/";
 
+const defaultOptions = {
+  theme: "white",
+};
+
 // リクエストパラメータを取得
 function getRequestParameters() {
   return location.search.substring(1).split("&").reduce((acc, cur) => {
@@ -9,27 +13,50 @@ function getRequestParameters() {
   }, {});
 };
 
-function getGHContents(owner, repo, path, token, callback, type = "text") {
-  // https://stackoverflow.com/a/42724593
+function getHttp(
+  url,
+  onload,
+  {
+    type = "text",
+    header = {},
+    onerror = undefined,
+  } = {}
+) {
   let xhr = new XMLHttpRequest();
-  xhr.responseType = type;
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-      callback(xhr);
+  xhr.addEventListener("load", () => onload(xhr));
+  xhr.addEventListener("error", () => {
+    if (onerror) {
+      onerror(xhr);
+    } else {
+      alert("Error: the server responded with " +
+	  `${xhr.status} ${xhr.statusText}`);
     }
-  };
-  xhr.open(
-      "GET",
-      `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-      true);
-  xhr.setRequestHeader("Authorization", `token ${token}`);
-  xhr.setRequestHeader("Accept", "application/vnd.github.v3.raw");
+  });
+  xhr.responseType = type;
+  xhr.open("GET", url);
+  for (let k in header) {
+    xhr.setRequestHeader(k, header[k]);
+  }
   xhr.send();
 }
 
-function decomposeGitHubURL(url) {
+function getGitHubContents(owner, repo, path, token, onload, type = "text") {
+  // https://stackoverflow.com/a/42724593
+  getHttp(
+    `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+    onload,
+    {
+      type: type,
+      header: {
+	Authorization: `token ${token}`,
+	Accept: "application/vnd.github.v3.raw",
+      },
+    });
+}
+
+function parseGitHubUrl(url) {
   const re = new RegExp(
-    "^https://github\.com/([^/]+)/([^/]+)/raw/([^/]+)/(.*)$");
+      "^https://github\.com/([^/]+)/([^/]+)/raw/([^/]+)/(.*)$");
   const mat = re.exec(url);
   if (mat.length == 5) {
     return {
@@ -43,32 +70,59 @@ function decomposeGitHubURL(url) {
   }
 }
 
-function showMarkdown(parameters, md) {
-  let markdownUrl = parameters["slide"];
-
-  let opt = {theme: "white"};
-
-  // Obtain the YAML front matter
+function separateFrontMatter(str) {
   const re = /\s*^---\s*$(.*?)^---\s*$(.*)/ms;
-
-  let mdbody = md;
-  if (md.search(re) == 0) {
-    const mat = re.exec(md);
+  // Since /^/m matches the beginning of each line, we check if the regex
+  // matches the beginning of the whole string.
+  if (str.search(re) == 0) {
+    const mat = re.exec(str);
     if (mat && mat.length == 3) {
-      opt = {...opt, ...jsyaml.load(mat[1])};
-      mdbody = mat[2];
+      return [jsyaml.load(mat[1]), mat[2]];
     }
   }
+  return [{}, str];
+}
 
-  if (opt.theme) {
+function applyOptions(opts) {
+  if (opts.theme) {
     let link = document.createElement("link");
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href = revealCdn + "css/theme/" + opt.theme + ".min.css";
+    link.href = revealCdn + "css/theme/" + opts.theme + ".min.css";
     document.getElementsByTagName("head")[0].appendChild(link);
   }
+}
 
-  // Slide指定
+function fixImagePath(imagePath, params, callback) {
+  // Fix img.src to relative path from index.html
+  const url = location.href.replace(/\?.*$/, "");
+  const mainPath = url.substring(0, url.lastIndexOf("/"));
+  const markDownDirPath =
+      params.slide.substring(0, params.slide.lastIndexOf("/"));
+  const relativePath = imagePath.split(mainPath)[1];
+  if (!relativePath && imagePath.match(/^(https?:)?\/\//)) {
+    callback(imagePath);
+  } else if ("token" in params) {
+    const repoinfo = parseGitHubUrl(markDownDirPath);
+    getGitHubContents(
+	repoinfo.owner,
+	repoinfo.repository,
+	`${repoinfo.path}/${imagePath}`,
+	params.token,
+	(xhr) => callback(URL.createObjectURL(xhr.response)),
+	"blob");
+  } else {
+    const result = relativePath ?
+	markDownDirPath + relativePath :
+	markDownDirPath + "/" + imagePath;
+    callback(result);
+  }
+}
+
+function showMarkdown(params, md) {
+  const [fm, mdbody] = separateFrontMatter(md);
+
+  applyOptions({...defaultOptions, ...fm});
   document.getElementById("markdown").innerHTML = mdbody;
 
   Reveal.initialize({
@@ -81,76 +135,48 @@ function showMarkdown(parameters, md) {
   }, false);
 
   Reveal.addEventListener("ready", (event) => {
-    // タイトル設定
+    // Use the first 'h1' element as a slide title
     document.title = document.getElementsByTagName("h1")[0].innerText;
-
-    // fix img.src to relative path from index.html
-    const url = location.href.replace(/\?.*$/,"");
-    const mainPath = url.substring(0, url.lastIndexOf("/"));
-    const markDownDirPath =
-        markdownUrl.substring(0, markdownUrl.lastIndexOf("/"));
-    Array.from(document.getElementsByTagName("img"), el => {
-      const imagePath = el.getAttribute("src");
-      const relativePath = imagePath.split(mainPath)[1];
-      if (!relativePath && imagePath.match(/^(https?:)?\/\//)) {
-        // another domain and absolute path case
-        return;
-      }
-      if ("token" in parameters) {
-        const repoinfo = decomposeGitHubURL(markDownDirPath);
-        getGHContents(
-            repoinfo.owner,
-            repoinfo.repository,
-            `${repoinfo.path}/${imagePath}`,
-            parameters.token,
-            (xhr) => el.setAttribute("src", URL.createObjectURL(xhr.response)),
-            "blob");
-      } else {
-        const result = relativePath ?
-            markDownDirPath + relativePath :
-            markDownDirPath + "/" + imagePath;
-        el.setAttribute("src", result);
-      }
-    });
+    // Fix img.src to relative path from index.html
+    for (let el of document.getElementsByTagName("img")) {
+      fixImagePath(
+	  el.getAttribute("src"), params, (url) => el.setAttribute("src", url));
+    }
   });
 }
 
-function getMarkdown(parameters) {
-  let xhr = new XMLHttpRequest();
-  xhr.addEventListener("error", () => {
-    const gh = decomposeGitHubURL(parameters.slide);
-    if (gh) {
-      if (!parameters.token) {
-        parameters.token = window.prompt("A network error occured. \n" +
-            "If the Markdown file is in a GitHub private repository, " +
-            "retry with a personal access token:");
-      }
-      if (parameters.token) {
-        getGHContents(gh.owner, gh.repository, gh.path, parameters.token,
-            (x) => showMarkdown(parameters, x.responseText));
-      }
-    }
-  });
-  xhr.addEventListener("load", () => {
-    if (xhr.status == 200) {
-      showMarkdown(parameters, xhr.responseText);
-    } else {
-      alert(`Error: the server responded with ${xhr.status} ` +
-          `${xhr.statusText}.`);
-    }
-  });
-  xhr.open("GET", parameters.slide);
-  xhr.send();
-};
-
 window.addEventListener("load", () => {
-  let parameters = getRequestParameters();
-  while (!parameters.slide) {
-    parameters.slide = window.prompt(
+  let params = getRequestParameters();
+  while (!params.slide) {
+    params.slide = window.prompt(
         "Input a Markdown URL",
         "https://araij.github.io/nammd/example/slide1.md");
   }
-  getMarkdown(parameters);
+  getHttp(
+    params.slide,
+    (xhr) => {
+      if (xhr.status == 200) {
+        showMarkdown(params, xhr.responseText);
+      } else {
+        alert(`Failed to get Markdown: ${xhr.status} ${xhr.statusText}.`);
+      }
+    },
+    {
+      onerror: (xhr) => {
+	const gh = parseGitHubUrl(params.slide);
+	if (gh) {
+	  if (!params.token) {
+	    params.token = window.prompt("A network error occured. \n" +
+		"If the Markdown file is in a GitHub private repository, " +
+		"retry with a personal access token:");
+	  }
+	  if (params.token) {
+	    getGitHubContents(gh.owner, gh.repository, gh.path, params.token,
+		(x) => showMarkdown(params, x.responseText));
+	  }
+	}
+      },
+    });
 });
 
 // Printing and PDF exports
