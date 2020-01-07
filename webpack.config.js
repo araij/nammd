58afa2635cb6db688f5c1697160905c676343838
araij <araijn@gmail.com>
2020-01-08 06:12:43 +0900

    Move to TypeScript
    ... but not yet typed

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+
+module.exports = {
+  entry: './src/nammd.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'nammd.js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  plugins: [
+    new LicenseWebpackPlugin({
+      outputFilename: './licenses.txt',
+      addBanner: true,
+      licenseFileOverrides: {
+	isarray: '../../licenses/isarray.txt',
+      },
+    })
+  ]
+};
+
